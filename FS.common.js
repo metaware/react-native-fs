36171fab025400541b6eb29a6eb586a8caf2ca20
Sandeep Bhatti <sandeep@metawarelabs.com>
2018-10-04 00:04:30 +0530

    new version of getFSInfo

--- FS.common.js
+++ FS.common.js
@@ -210,6 +210,10 @@ var RNFS = {
     return RNFSManager.getFSInfo();
   },
 
+  getFSInfoPath(filepath: string): Promise<FSInfoResult> {
+    return RNFSManager.getFSInfoPath(filepath);
+  },
+
   getAllExternalFilesDirs(): Promise<string> {
     return RNFSManager.getAllExternalFilesDirs();
   },
